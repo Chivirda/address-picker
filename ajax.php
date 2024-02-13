314e2145f076dd7c6273f136a6e023438b621547
Sergey Chivirda <si.chivirda@gmail.com>
2024-02-13 20:10:52 +0300

    feat: print region, district, settlements & streets

--- ajax.php
+++ ajax.php
@@ -1,20 +1,31 @@
 <?php
 
+const REGION_INDEX = 2;
+const DISTRICT_INDEX = 3;
+const CITY_INDEX = 5;
+const STREET_INDEX = 6;
+
+// echo '<pre>';
+// print_r($addresses);
+// echo '</pre>';
+// die();
+
 if (isset($_POST['action'])) {
     $action = $_POST['action'];
 
     switch ($action) {
-        case 'get_cities':
-            echo json_encode(getCities());
+        case 'get_region':
+            echo json_encode(getRegion());
+            break;
+        case 'get_districts':
+            echo json_encode(getDistrict());
+            break;
+        case 'get_settlements':
+            echo json_encode(getSettlements());
             break;
         case 'get_streets':
-            $city = $_POST['city'];
-            echo json_encode(getStreets($city));
-            break;
-        case 'filter_addresses':
-            $city = $_POST['city'];
-            $street = $_POST['street'];
-            echo json_encode(filterAddresses($city, $street));
+            $settlement = $_POST['settlement'];
+            echo json_encode(getStreets($settlement));
             break;
     }
 }
@@ -37,9 +48,47 @@ function readAddressesFromCSV($filename)
     return $addresses;
 }
 
+function getColumnUniqueValues(int $columnIndex): array
+{
+    $addresses = readAddressesFromCSV('addreses.csv');
 
-$addresses = readAddressesFromCSV('addreses.csv');
+    $values = [];
 
-echo "<pre>";
-print_r($addresses);
-echo "</pre>";
+    foreach ($addresses as $address) {
+        if (count($address) > 1) { // Skip the header row if it exists
+            $values[] = $address[$columnIndex];
+        }
+    }
+
+    return array_unique($values);
+}
+
+function getRegion(): array
+{
+    return getColumnUniqueValues(REGION_INDEX);
+}
+
+function getDistrict(): array
+{
+    return getColumnUniqueValues(DISTRICT_INDEX);
+}
+
+function getSettlements(): array
+{
+    return getColumnUniqueValues(CITY_INDEX);
+}
+
+function getStreets(string $city): array
+{
+    $addresses = readAddressesFromCSV('addreses.csv');
+
+    $values = [];
+
+    foreach ($addresses as $address) {
+        if (count($address) > 1 && $address[CITY_INDEX] === $city) {
+            $values[] = $address[STREET_INDEX];
+        }
+    }
+
+    return array_unique($values);
+}
--- script.js
+++ script.js
@@ -1,78 +1,83 @@
 document.addEventListener('DOMContentLoaded', async function () {
     // Загрузка населенных пунктов при открытии страницы
-    await loadCities();
+    await loadRegion();
 
-    async function request(url, data) {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            body: new URLSearchParams(data)
-        });
-
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    }
-
-    async function loadCities() {
-        try {
-            const data = await request('ajax.php', {action: 'get_cities'});
-            populateDropdown('city', data);
-        } catch (error) {
-            console.error('Error loading cities:', error);
-        }
-    }
-
-    async function loadStreets() {
-        const selectedCity = document.getElementById('city').value;
-        try {
-            const data = await request('ajax.php', {action: 'get_streets', city: selectedCity});
-            populateDropdown('street', data);
-        } catch (error) {
-            console.error('Error loading streets:', error);
-        }
-    }
-
-    async function filterAddresses() {
-        const selectedCity = document.getElementById('city').value;
-        const selectedStreet = document.getElementById('street').value;
-        try {
-            const data = await request('ajax.php', {
-                action: 'filter_addresses',
-                city: selectedCity,
-                street: selectedStreet
-            });
-            displayAddresses(data);
-        } catch (error) {
-            console.error('Error filtering addresses:', error);
-        }
-    }
-
-    function populateDropdown(elementId, data) {
-        const dropdown = document.getElementById(elementId);
-        dropdown.innerHTML = '';
-        data.forEach(function (value) {
-            const option = document.createElement('option');
-            option.value = value;
-            option.textContent = value;
-            dropdown.appendChild(option);
-        });
-    }
-
-    function displayAddresses(data) {
-        const container = document.getElementById('addresses-container');
-        container.innerHTML = '';
-        data.forEach(function (value) {
-            const p = document.createElement('p');
-            p.textContent = value;
-            container.appendChild(p);
-        });
-    }
-
-    // Event listeners for dropdown changes
-    document.getElementById('city').addEventListener('change', loadStreets);
-    document.querySelector('button').addEventListener('click', filterAddresses);
 });
+async function request(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(data)
+    });
 
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+}
+
+async function loadRegion() {
+    try {
+        const data = await request('ajax.php', { action: 'get_region' });
+        const region = document.querySelector('#region');
+        region.innerHTML = data
+        await loadDistricts();
+    } catch (error) {
+        console.error('Error loading region:', error);
+    }
+}
+
+async function loadDistricts() {
+    try {
+        const data = await request('ajax.php', { action: 'get_districts' });
+        populateDropdown('district-list', data);
+    } catch (error) {
+        console.error('Error loading districts:', error);
+    }
+}
+
+async function loadSettlements() {
+    try {
+        const data = await request('ajax.php', { action: 'get_settlements' });
+        populateDropdown('settlement-list', Object.values(data));
+    } catch (error) {
+        console.error('Error loading settlements:', error);
+    }
+}
+
+async function loadStreets() {
+    const selectedSettlement = document.getElementById('settlement').value;
+    try {
+        const data = await request('ajax.php', { action: 'get_streets', settlement: selectedSettlement });
+        populateDropdown('street-list', Object.values(data));
+    } catch (error) {
+        console.error('Error loading streets:', error);
+    }
+}
+
+function populateDropdown(elementId, data) {
+    const dropdown = document.getElementById(elementId);
+    dropdown.innerHTML = '';
+    data.forEach(function (value) {
+        const option = document.createElement('option');
+        option.value = value;
+        option.textContent = value;
+        dropdown.appendChild(option);
+    });
+}
+
+function displayAddresses(data) {
+    const container = document.getElementById('addresses-container');
+    container.innerHTML = '';
+    data.forEach(function (value) {
+        const p = document.createElement('p');
+        p.textContent = value;
+        container.appendChild(p);
+    });
+}
+
+// Event listeners for dropdown changes
+document.getElementById('district').addEventListener('change', loadSettlements);
+document.getElementById('settlement').addEventListener('change', loadStreets);
+document.querySelector('button').addEventListener('click', filterAddresses);
